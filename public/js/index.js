a8b21a18169eab8bdfdd185e88382a970340388c
Rodrigo Martins <contawarface750@gmail.com>
2017-09-25 00:57:52 -0300

    #4

--- public/js/index.js
+++ public/js/index.js
@@ -12,9 +12,14 @@ function ajax(url, callback) {
 
 var busca = (palavra)=> ajax(`http://dicionario-aberto.net/search-json/${palavra}`, 
         function (e) {
-            var significados = JSON.parse(e.target.response);
-            var def = significados.entry.sense[0].def;
+            var def = (JSON.parse(e.target.response));//.entry.sense[0].def;
+            var definicao = '';
+            (def.superEntry)? (definicao += def.superEntry[0].entry.sense[0].def+'<br/>', 
+            definicao += def.superEntry[1].entry.sense[0].def) : definicao += def.entry.sense[0].def;
+
+            while (definicao.indexOf('_') != -1) { definicao = definicao.replace('_',''); }
+            
             document.querySelector('#select').value = palavra;
             document.querySelector('#definicao').value = '';
-            document.querySelector('#definicao').innerHTML = def;
+            document.querySelector('#definicao').innerHTML = definicao;
         });
