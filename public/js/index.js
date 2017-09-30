f555d305635a551647a58963fd4fec9ad0f72da8
Rodrigo Martins <contawarface750@gmail.com>
2017-09-30 15:53:56 -0300

    #5

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,36 @@
-document.querySelector('#teste').addEventListener('click', function(e) {
-  busca(document.querySelector('#palavra').value);
+var findWord = function () {
+  var textarea = document.querySelector('textarea');
+  var pos = textarea.selectionStart;
+  var palavras = textarea.value.split(' ');
+  var palavra;
+  for (var i = qnt = 0; (i < palavras.length && !palavra); i++) {
+    qnt += palavras[i].length+1;
+    if (qnt > pos && !palavra) palavra = palavras[i];
+  }
+  return palavra
+}
+
+var buildMeaning = function (significado, string) {
+  var def = significado;
+  var definicao = '';
+  if (def.superEntry) {
+      for (var i = 0; i < def.superEntry.length; i++){
+        definicao += def.superEntry[i].entry.sense[0].def+'<br/>';
+      }
+  } else definicao += def.entry.sense[0].def;
+
+  while (definicao.indexOf('_') != -1) { 
+    definicao = definicao.replace('_',''); 
+  }            
+  document.querySelector('#select').value = string;
+  document.querySelector('#definicao').value = '';
+  document.querySelector('#definicao').innerHTML = definicao;
+}
+
+
+document.querySelector('#palavra').addEventListener('click', function (e) {
+  var temp = findWord();
+  busca(temp);
   e.preventDefault();
 });
 
@@ -10,16 +41,4 @@ function ajax(url, callback) {
   xhr.send();
 }
 
-var busca = (palavra)=> ajax(`http://dicionario-aberto.net/search-json/${palavra}`, 
-        function (e) {
-            var def = (JSON.parse(e.target.response));//.entry.sense[0].def;
-            var definicao = '';
-            (def.superEntry)? (definicao += def.superEntry[0].entry.sense[0].def+'<br/>', 
-            definicao += def.superEntry[1].entry.sense[0].def) : definicao += def.entry.sense[0].def;
-
-            while (definicao.indexOf('_') != -1) { definicao = definicao.replace('_',''); }
-            
-            document.querySelector('#select').value = palavra;
-            document.querySelector('#definicao').value = '';
-            document.querySelector('#definicao').innerHTML = definicao;
-        });
+var busca = (palavra)=> ajax(`http://dicionario-aberto.net/search-json/${palavra}`, (e)=> buildMeaning(JSON.parse(e.target.response), palavra));
